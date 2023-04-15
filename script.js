249a2ec2a2dcce5554c0c58be5148f942a2f3eb3
LoNGER-2111 <96116356+LoNGER-2111@users.noreply.github.com>
2023-04-15 21:25:34 +0700

    commit changes

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ const fullname = $("#fullname");
 const department = $("#department");
 const coeffitSalary = $("#coefficients-salary");
 const saveInfoBtn = $("#save-info");
-const calSalaryBtn = $("#calculate-salary");
 const updateInfoBtn = $("#update-info");
 const table = $(".table");
 
-let staffList = [
+const staffList = [
   {
     fullname: "Phạm Thành Long",
     department: "Kĩ sư phần mềm",
@@ -20,13 +19,14 @@ let staffList = [
 ];
 
 const basicSalaryValue = 2000000;
+let selectedIndex = 0;
 
 function renderInfo() {
-  basicSalary.value = basicSalaryValue.toLocaleString("en-US", {
-    useGrouping: true,
-  });
+  for (let i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
   for (let i = 0; i < staffList.length; i++) {
-    const newRow = table.insertRow(table.length);
+    const newRow = table.insertRow(table.rows.length);
 
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
@@ -43,14 +43,15 @@ function renderInfo() {
       staffList[i].coeffitSalary * basicSalaryValue
     ).toLocaleString("en-US", { useGrouping: true });
     cell6.innerHTML = `
-    <a href="javascript:void(0)" id="edit-btn">Sửa</a> / 
-    <a href="javascript:void(0)" id="delete-btn">Xóa</a>
+    <a href="javascript:void(0)" class="edit-btn" onclick="editInfo(this)">Sửa</a> / 
+    <a href="javascript:void(0)" class="delete-btn" onclick="deleteInfo(this)">Xóa</a>
     `;
   }
+  clearForm();
 }
 
 function saveInfo() {
-  const staffInfo = {
+  let staffInfo = {
     fullname: fullname.value,
     department: department.value,
     coeffitSalary: coeffitSalary.value,
@@ -59,7 +60,50 @@ function saveInfo() {
   renderInfo();
 }
 
+function editInfo(e) {
+  let selectedRow = e.parentElement.parentElement;
+
+  fullname.value = selectedRow.cells[1].innerText;
+  department.value = selectedRow.cells[2].innerText;
+  coeffitSalary.value = selectedRow.cells[3].innerText;
+
+  selectedIndex = parseInt(selectedRow.cells[0].innerText) - 1;
+
+  updateInfoBtn.classList.remove("hidden");
+  saveInfoBtn.classList.add("hidden");
+}
+
+function updateInfo() {
+  staffList[selectedIndex].fullname = fullname.value;
+  staffList[selectedIndex].department = department.value;
+  staffList[selectedIndex].coeffitSalary = coeffitSalary.value;
+  renderInfo();
+
+  updateInfoBtn.classList.add("hidden");
+  saveInfoBtn.classList.remove("hidden");
+}
+
+function deleteInfo(e) {
+  let selectedRow = e.parentElement.parentElement;
+  selectedIndex = parseInt(selectedRow.cells[0].innerText) - 1;
+  if (confirm("Bạn có muốn xóa dữ liệu nhân viên này không?")) {
+    staffList.splice(selectedIndex, 1);
+  }
+  renderInfo();
+}
+
+function clearForm() {
+  fullname.value = "";
+  department.value = "";
+  coeffitSalary.value = "";
+}
+
+basicSalary.value = basicSalaryValue.toLocaleString("en-US", {
+  useGrouping: true,
+});
+
 form.addEventListener("submit", (e) => e.preventDefault());
 saveInfoBtn.addEventListener("click", saveInfo);
+updateInfoBtn.addEventListener("click", updateInfo);
 
 renderInfo();
